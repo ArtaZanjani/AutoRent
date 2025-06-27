76655baa0a89ef8b5615c445ad47e0add83baac6
M7a1s <yoarta87@gmail.com>
2025-06-28 00:28:12 +0330

    Updates

---
+++ assets/FetchError-AuI-kfe3.js
@@ -0,0 +1,1 @@
+import{c as a,t as c,d as n,u as f,r as i,j as l}from"./index-DcONTQop.js";import{F as u}from"./FetchErrorComp-DH56V-qB.js";const p=()=>{const{clearErrors:o}=a(),{setIsScrolling:r}=c(),t=n(),s=f();i.useEffect(()=>{t.pathname==="/fetcherror"&&r(!1)},[t.pathname,r]);const e=()=>{o(),s("/")};return l.jsx(u,{refetch:e})};export{p as default};
